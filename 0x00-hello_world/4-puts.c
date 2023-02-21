9277b2b1a8d198c1f7dea01790a0a4bbe327e099
tshidiso10 <tshidisomakinta@gmail.com>
2023-02-20 19:34:14 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - a programm that prints a line with function
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
