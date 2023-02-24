b45e34760121fe6f7e2523f074778690c4570ac6
tshidiso10 <tshidisomakinta@gmail.com>
2023-02-23 20:20:34 -0800

    more functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *more_numbers - print more numbers
+ * more_numbers - print more numbers
  */
 
 void more_numbers(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 1; i <= 10; i++)
 	{
