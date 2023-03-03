d724fe2dacf681ff4228c5248b976696f0766579
Tshidiso10 <tshidisomakinta@gmail.com>
2023-03-03 03:12:57 +0200

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *  * string_toupper - change all lowercase to uppercase
- *   * @n: pointer
- *    *
- *     * Return: n
- *      */
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
 char *string_toupper(char *n)
 {
 		int i;
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * leet - encode into 1337speak
- *   * @n: input value
- *    * Return: n value
- *     */
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
 char *leet(char *n)
 {
 		int i, j;
