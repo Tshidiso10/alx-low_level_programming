35e434944aa7ad70d805f6205587b180966ce16f
Tshidiso10 <tshidisomakinta@gmail.com>
2023-07-07 13:23:09 -0700

    stat

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h> 
+ 
+/** 
+ * _putchar - writes char c to stdout 
+ * @c: Character to print 
+ * 
+ * Return: On success 1, 
+ * if fail, returned -1 
+ */ 
+int _putchar(char c) 
+{ 
+ return (write(1, &c, 1)); 
+} 
