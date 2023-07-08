aaf4c553f9e77c5dac44fff04842e022a6c5c876
Tshidiso10 <tshidisomakinta@gmail.com>
2023-07-07 17:11:31 -0700

    argsv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - print the number of arguments passed
+ * @argc: arguments counter
+ * @argv: stores arguments in array 
+ *
+ * Return: Always 0 
+ */
+
 int main (int argc, char **argv[])
 {
  	 (void) argv;
