15885bfd1f031cc18043ce3d6be6ffb97f76cb56
Tshidiso10 <tshidisomakinta@gmail.com>
2023-07-07 17:20:02 -0700

    argsv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -13,11 +13,7 @@
 
 int main (int argc, char **argv[])
 {
-    int i;
-    
-    for (i = 0; i < argc; i++)
-    {
-        printf("%s", *argv[i])
-    }
+        printf("%s", *argv)
+
     return 0;
 }
