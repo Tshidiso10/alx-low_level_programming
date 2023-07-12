95f172e5a263076b5d2dda89771441d92faf2b70
Tshidiso10 <tshidisomakinta@gmail.com>
2023-07-11 18:57:28 -0700

    maloko

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
     char *array;
     size_t i;
 
-    array = (char*) malloc(size*sizeof(char));
+    array = malloc(size*sizeof(char));
 
     if(size == 0)
         return NULL;
