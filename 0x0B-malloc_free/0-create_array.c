e747a011c698e05305e30db555ad3e49030bc28f
Tshidiso10 <tshidisomakinta@gmail.com>
2023-07-11 18:53:42 -0700

    maloko

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - create array of size size 
+ * @size: size of array
+ * @c: char to assign
+ * Return: pointer to array, 
+ * NULL if fail
+ */
+
+
+char *create_array(unsigned int size, char c)
+{
+    char *array;
+    size_t i;
+
+    array = (char*) malloc(size*sizeof(char));
+
+    if(size == 0)
+        return NULL;
+
+    if(array == NULL)
+        return NULL;
+
+    for (i = 0; i < size; i++)
+		array[i] = c;
+
+    return (array);
+}
